d901d19da218542f58c8cf8cbc90b76c015d266d
K-Maxim <101186665+K-Maxim@users.noreply.github.com>
2022-05-19 08:32:24 +0300

    Основной код программы

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_restx import Api
+from config import Config
+from setup_db import db
+from views.director import director_ns
+from views.genre import genre_ns
+from views.movies import movie_ns
+
+
+def create_app(config):  # конфигурация БД
+    application = Flask(__name__)
+    application.config.from_object(config)
+    application.app_context().push()
+    configure_app(application)
+    return application
+
+
+def configure_app(app):  # конфигурируем приложение
+    db.init_app(app)
+    api = Api(app)
+    api.add_namespace(movie_ns)
+    api.add_namespace(genre_ns)
+    api.add_namespace(director_ns)
+    # create_data(app, db)
+
+
+#
+#
+# функция
+# def create_data(app, db):
+#     with app.app_context():
+#         db.create_all()
+#
+#         создать несколько сущностей чтобы добавить их в БД
+#
+#         with db.session.begin():
+#             db.session.add_all(здесь список созданных объектов)
+#
+#
+
+
+if __name__ == '__main__':
+    app_config = Config()  # загружаем конфигурацию
+    app = create_app(app_config)  # создаем приложение
+    configure_app(app)  # конфигурируем приложение
+    app.run(host="localhost")  # запускаем приложение
